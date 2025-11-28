d9dd7ea30ed43390b1d4e997ccacd89c05276c1c
PrashantGoyal20 <prashant.goyal2002@gmail.com>
2025-11-28 18:11:56 +0530

    initial commit

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, HttpUrl
 import uuid
-from src.extractor import stream_to_s3,start_textract_job,poll_textract,parse_tables,PREFIX,BUCKET
+# from src.extractor import stream_to_s3,start_textract_job,poll_textract,parse_tables,PREFIX,BUCKET
 
 app = FastAPI()
 
